76e069ec00e8003d8ad89a9d330273797323d670
codingspecialist <codingspecialist@naver.com>
2024-10-04 09:11:22 +0900

    크롬 드라이버 업데이트

--- oliveyoung_craw.py
+++ oliveyoung_craw.py
@@ -9,11 +9,8 @@ import pandas as pd
 
 
 def set_chrome_driver():
-    chrome_options = webdriver.ChromeOptions()
-    chrome_options.add_experimental_option(
-        "excludeSwitches", ["enable-logging"])
-    driver = webdriver.Chrome(service=Service(
-        ChromeDriverManager().install()), options=chrome_options)
+
+    driver = webdriver.Chrome(service=Service())
     return driver
 
 
